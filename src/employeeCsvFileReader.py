404df35c6103be591845713bf1b945fa02c5d493
guhabiswasavijit <guhabiswas.avijit@gmail.com>
2021-12-09 11:41:10 +0530

    reading sql from file

--- src/employeeCsvFileReader.py
+++ src/employeeCsvFileReader.py
@@ -1,7 +1,7 @@
 import csv
 import employee as emp
 import mysql.connector
-
+from bs4 import BeautifulSoup
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -9,12 +9,17 @@ mydb = mysql.connector.connect(
   password="Lotus2ibm",
   database = "musical"
 )
+with open('sql_queries.xml', 'r') as f:
+    data = f.read()
+bs_data = BeautifulSoup(data, 'xml')
+sqlElement = bs_data.find('sql')
+sql = sqlElement.text
+print("got query string {}",sql)
 mycursor = mydb.cursor()
 with open('C:\\SampleData\\employees.csv', newline='\n') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         data = emp.Employee(row['EMPLOYEE_ID'],row['FIRST_NAME'],row['LAST_NAME'],row['EMAIL'],row['PHONE_NUMBER'],row['HIRE_DATE'],row['JOB_ID'],row['SALARY'],row['COMMISSION_PCT'],row['MANAGER_ID'],row['DEPARTMENT_ID'])
-        sql = "INSERT INTO EMPLOYEE(EMPLOYEE_ID,FIRST_NAME,LAST_NAME,EMAIL,PHONE_NUMBER,HIRE_DATE,JOB_ID,SALARY,COMMISSION_PCT,MANAGER_ID,DEPARTMENT_ID) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         val = (data.employeeId,data.firstName,data.lastName,data.email,data.phoneNumber,data.hireDate,data.jobId,data.salary,data.commisionPct,data.managerId,data.departmentId)
         mycursor.execute(sql, val)
 mydb.commit()
